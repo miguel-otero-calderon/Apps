4756e5e66a1ee73992f2af43f39d254a7c991a6f
miguel-otero-calderon@hotmail.com <miguel-otero-calderon@hotmail.com>
2017-10-02 00:42:11 -0500

    SCI

---
+++ Apps/Apps.Web.SCI/Controllers/UtilitarioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Apps.Web.SCI.Controllers
+{
+    public class UtilitarioController : Controller
+    {
+        // GET: Utilitario
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
