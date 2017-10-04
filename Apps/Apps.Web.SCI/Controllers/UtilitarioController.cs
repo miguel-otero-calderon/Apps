7cd822df98e349cb41255506a591295bf3267e78
miguel.otero <miguel-otero-calderon@hotmail.com>
2017-10-04 18:59:33 -0500

    Load Files

--- Apps/Apps.Web.SCI/Controllers/UtilitarioController.cs
+++ Apps/Apps.Web.SCI/Controllers/UtilitarioController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace Apps.Web.SCI.Controllers
 {
@@ -13,5 +14,53 @@ namespace Apps.Web.SCI.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public ActionResult UploadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+                    string _FileName = Path.GetFileName(file.FileName);
+                    string _path = Path.Combine(Server.MapPath("~/UploadedFiles"), _FileName);
+                    file.SaveAs(_path);
+                }
+                ViewBag.Message = "File Uploaded Successfully!!";
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "File upload failed!!";
+                return View();
+            }
+        }
+
+        //[HttpPost]
+        //public JsonResult LoadFile(HttpPostedFileBase file)
+        //{
+        //    try
+        //    {
+        //        if (file.ContentLength > 0)
+        //        {
+        //            string _FileName = Path.GetFileName(file.FileName);
+        //            string _path = Path.Combine(Server.MapPath("~/UploadedFiles"), _FileName);
+        //            file.SaveAs(_path);
+        //        }
+        //        ViewBag.Message = "File Uploaded Successfully!!";
+        //        return View();
+        //    }
+        //    catch
+        //    {
+        //        ViewBag.Message = "File upload failed!!";
+        //        return View();
+        //    }
+        //}
     }
 }
--- Apps/Apps.Web.SCI/Scripts/Utilitario/index.js
+++ Apps/Apps.Web.SCI/Scripts/Utilitario/index.js
@@ -1,1 +1,24 @@
-﻿
+﻿$(document).ready(function () {  
+    var fecha = "";
+    fecha = obtener_fecha_hoy();
+    $("#date1").val(fecha);
+})
+
+function obtener_fecha_hoy() {
+    var f = new Date();
+    var day = f.getDate();
+    var month = f.getMonth() + 1;
+    var year = f.getFullYear();
+    var fecha = "";
+
+    if(day < 10)
+        day = "0" + day;
+
+    if(month < 10)
+        month = "0" + month;
+
+    fecha = year + "-" + month + "-" + day;
+
+    return fecha;
+
+}
