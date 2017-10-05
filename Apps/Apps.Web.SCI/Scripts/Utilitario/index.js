4b6827d3d25cdd9b1f74ec84d2f6f3d1f52b6cc3
miguel-otero-calderon@hotmail.com <miguel-otero-calderon@hotmail.com>
2017-10-05 02:28:45 -0500

    validar date

--- Apps/Apps.Web.SCI/Scripts/Utilitario/index.js
+++ Apps/Apps.Web.SCI/Scripts/Utilitario/index.js
@@ -1,10 +1,10 @@
-﻿$(document).ready(function () {  
-    var fecha = "";
-    fecha = obtener_fecha_hoy();
-    $("#date1").val(fecha);
+﻿$(document).ready(function () {      
+    $("#date1").val(obtener_fecha_hoy());
+    $("#error").val("");
+
 })
 
-function obtener_fecha_hoy() {
+var obtener_fecha_hoy = function() {
     var f = new Date();
     var day = f.getDate();
     var month = f.getMonth() + 1;
@@ -20,5 +20,87 @@ function obtener_fecha_hoy() {
     fecha = year + "-" + month + "-" + day;
 
     return fecha;
+}
 
+var validar_fecha = function (fecha) {
+    try
+    {
+        if (validate_fecha_formato_ingles(fecha))
+            return true;
+        else
+            return false;
+    }
+    catch (err)
+    {
+        return false;
+    }
+
+}
+
+var validar_load_files = function () {
+    var texto;
+    var value;
+    var file;
+
+    $("#error").html("");
+    texto = $("#date1").val();
+    value = validar_fecha(texto);
+    if (value == false)
+    {
+        texto = "Digite una fecha con el formato 'yyyy-mm-dd'.";
+        $("#error").html(texto);
+        $("#date1").focus();
+        return false;
+    }
+
+    if (validate_file("file1") == false)
+        return false;
+
+    if (validate_file("file2") == false)
+        return false;
+
+    return true;
+}
+
+function isValidDate(day, month, year) {
+    var dteDate;
+    month = month - 1;
+    dteDate = new Date(year, month, day);
+    return ((day == dteDate.getDate()) && (month == dteDate.getMonth()) && (year == dteDate.getFullYear()));
+}
+
+function validate_fecha_formato_ingles(fecha) {
+    var patron = new RegExp("^(19|20)+([0-9]{2})([-])([0-9]{1,2})([-])([0-9]{1,2})$");
+
+    if (fecha.search(patron) == 0) {
+        var values = fecha.split("-");
+        if (isValidDate(values[2], values[1], values[0])) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function validate_file(file_id) {
+    var texto;
+    var file = $("#" + file_id);
+    var date = $("#date1").val();
+    if (file.val() == "")
+    {
+        texto = "Falta seleccionar el " + file_id;
+        $("#error").html(texto);
+        $(file).focus();
+        return false;
+    }
+    else {
+        texto = file.val();
+        if (texto.endsWith(date + ".csv"))
+            return true;
+        else {
+            texto = "Nombre de archivo incorrecto...Porqye no termina en '" + date + "'";
+            $("#error").html(texto);
+            $(file).focus();
+            return false;
+        }
+    }
 }
